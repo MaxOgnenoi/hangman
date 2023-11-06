cbddc851c8c19c8d238271941e44957c64f02640
Max Ognenoi <maksymohnenyi>
2023-11-06 00:17:18 -0500

    finished main code

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,10 @@
 type HangmanDrawingProps = {
     guessedLetters: string[]
     wordToGuess: string
+    reveal?: boolean
 }
 
-export function HangmanWord({ guessedLetters, wordToGuess }: HangmanDrawingProps) {
+export function HangmanWord({ guessedLetters, wordToGuess, reveal = false }: HangmanDrawingProps) {
 
     return <div style={{
         display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace"
@@ -12,7 +13,9 @@ export function HangmanWord({ guessedLetters, wordToGuess }: HangmanDrawingProps
             <span style={{ borderBottom: ".1em solid black" }} key={index}>
                 <span
                     style={{
-                        visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+                        visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
+                        color:
+                            !guessedLetters.includes(letter) && reveal ? "red" : "black",
                     }}>
                     {letter}
                 </span>
