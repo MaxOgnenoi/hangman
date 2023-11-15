65ba895060e91709d958e248ffd7c1b2754a0644
Max Ognenoi <maksymohnenyi>
2023-11-14 23:41:33 -0500

    fix small screens

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,25 +1,29 @@
-type HangmanDrawingProps = {
-    guessedLetters: string[]
-    wordToGuess: string
-    reveal?: boolean
-}
+// HangmanWord.tsx
 
-export function HangmanWord({ guessedLetters, wordToGuess, reveal = false }: HangmanDrawingProps) {
+import React from "react";
+import styles from "./HangmanWord.module.css";
 
-    return <div style={{
-        display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace"
-    }}>
-        {wordToGuess.split("").map((letter, index) => (
-            <span style={{ borderBottom: ".1em solid #AE445A" }} key={index}>
-                <span
-                    style={{
-                        visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
-                        color:
-                            !guessedLetters.includes(letter) && reveal ? "red" : "#E8BCB9",
-                    }}>
-                    {letter}
-                </span>
-            </span>
-        ))}
+type HangmanWordProps = {
+  guessedLetters: string[];
+  wordToGuess: string;
+  reveal?: boolean;
+};
+
+export function HangmanWord({ guessedLetters, wordToGuess, reveal = false }: HangmanWordProps) {
+  return (
+    <div className={styles.hangmanWord}>
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid #AE445A" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
+              color: !guessedLetters.includes(letter) && reveal ? "red" : "#E8BCB9",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
     </div>
+  );
 }
